8ef0bd112793ca2035500422149485c4dfe0d26b
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-04-03 23:58:42 +0600

    App

--- 01/reactmastery/src/App.jsx
+++ 01/reactmastery/src/App.jsx
@@ -1,15 +1,15 @@
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx";
 
 function App() {
   return (
-
     <>
-    <Header/>
-    <Footer/>
-
+      <Header />
+      <Food />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
