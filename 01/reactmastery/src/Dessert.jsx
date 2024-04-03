3e350f2ccef07d34b5fb031070ed1e0d40241f88
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-04-04 00:05:39 +0600

    Dessert

---
+++ 01/reactmastery/src/Dessert.jsx
@@ -0,0 +1,12 @@
+function Dessert() {
+    return(
+        <div>
+            <ul>
+                <li><a href="#">Cake</a></li>
+                <li><a href="#">Pie</a></li>
+                <li><a href="#">Cookie</a></li>
+            </ul>
+        </div>
+    )
+}
+export default Dessert
