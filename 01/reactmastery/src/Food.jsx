3d4174a4018a71a28074f02f2f1bf5ee87660bfd
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-04-03 23:56:24 +0600

    food

---
+++ 01/reactmastery/src/Food.jsx
@@ -0,0 +1,21 @@
+function Food() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="#">Apple</a>
+        </li>
+        <li>
+          <a href="#">Banana</a>
+        </li>
+        <li>
+          <a href="#">Coconut</a>
+        </li>
+        <li>
+          <a href="#">Kiwi</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+export default Food
