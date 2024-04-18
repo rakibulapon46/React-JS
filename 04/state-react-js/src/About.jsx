45f15169f085b39f2c1d12bbc3c95d725aa1e553
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-04-18 17:10:09 +0600

    about jsx

--- 04/state-react-js/src/About.jsx
+++ 04/state-react-js/src/About.jsx
@@ -6,20 +6,39 @@ class About extends Component {
   //     age: 18,
   //     profession: "Freelance"
   // }
-  
+
   //todo  alternative
   constructor(props) {
     super(props);
     this.state = {
       name: "Apon",
       age: 18,
-      profession: "Freelance",
+      profession: "Web Developer"
+      // profession: {
+      //   prof1:"Freelance",
+      //   prof2: "Web application developer",
+      //   prof3: "Content creating"
+      // }
     };
   }
+
+  ChangeValue(a){
+    this.setState({
+      profession: "Freelance"
+    })
+  }
+
+  ChangeMind = this.ChangeValue.bind(this, this.a)
+
   render() {
     return (
       <>
-        <h1>{"I'm " + this.state.name + ". I'm " + this.state.age + ". My profession is " + this.state.profession +"."}</h1>
+        {/* <h1>{"I'm " + this.state.name + ". I'm " + this.state.age + ". My profession is " + this.state.profession +"."}</h1> */}
+
+        {/* <h1>My profession is {this.state.profession.prof2}</h1> */}
+
+        <h1>My profession is {this.state.profession}</h1>
+        <button onClick={this.ChangeMind}>Change Profession</button>
       </>
     );
   }
