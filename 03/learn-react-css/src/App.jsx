f693aa310ac7c01518577e791c980eda72fd11c8
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-04-19 23:37:03 +0600

    app jsx

--- 03/learn-react-css/src/App.jsx
+++ 03/learn-react-css/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Learn from './learn.jsx'
+import Button from './Button/Button.jsx'
 
 function App() {
 
@@ -10,6 +11,9 @@ function App() {
       <Learn name="Antor" number="0002" />
       <Learn name="Rakibul" number="0003" />
       <Learn name="Jubaer" number="0004" />
+      <Learn/>
+      <hr />
+      <Button/>
     </>
   )
 }
