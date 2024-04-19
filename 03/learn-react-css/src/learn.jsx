f8a2a343565ce61fc576389eeaa1732f07fefe1b
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-04-19 23:36:48 +0600

    learn jsx

--- 03/learn-react-css/src/learn.jsx
+++ 03/learn-react-css/src/learn.jsx
@@ -1,8 +1,20 @@
 
 function Learn(ContactProps) {
+  let styles = {
+      fontSize: '20px',
+      padding: '10px 20px',
+      backgroundColor: '#41c2fe',
+      color: 'white',
+      borderRadius: '5px',
+      border: '2px solid #41c2fe',
+      cursor:' pointer',
+  }
   return (
    <>
-   <div>My name is {ContactProps.name} and my number is {ContactProps.number} </div>
+   <div>My name is {ContactProps.name} and my number is {ContactProps.number} 
+<br />
+<button style={styles}>Click here</button>
+</div>
    </>
   )
 }
