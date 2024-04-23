9c5321f28c1adad17bd2eb0f255ade337dc44ac9
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-04-23 21:37:58 +0600

    clipboard jsx

--- 06/events-method/src/Clipboard.jsx
+++ 06/events-method/src/Clipboard.jsx
@@ -1,9 +1,30 @@
 function Clipboard() {
+  const copyText = () => {
+    alert("Text copied");
+  };
+  const onCutText = () => {
+    console.log("Text cutted successfully");
+  };
+  const onPasteText = () => {
+    let myInput = document.getElementById('myInput');
+    setTimeout(() => {
+      alert(myInput.value)
+    }, 2000);
+  }
   return (
     <>
-    <h1>hi</h1>
+      <p onCopy={copyText}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem,
+        nobis illo. Dolores quaerat quia totam ipsa nobis explicabo, earum
+        tempore.
+      </p>
+      <h2 onCut={onCutText}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, est.
+      </h2>
+      <input type="text" onPaste={onPasteText} id="myInput" placeholder="Enter your text here"/>
+      
     </>
-  )
+  );
 }
 
-export default Clipboard
+export default Clipboard;
