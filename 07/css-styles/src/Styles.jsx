4bba90749f988a0913205e03465dcbe039eb4e1b
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-05-02 22:36:25 +0600

    style jsx

--- 07/css-styles/src/Styles.jsx
+++ 07/css-styles/src/Styles.jsx
@@ -1,13 +1,18 @@
+import './Style.css'
+
 function Styles() {
   const ElementOne = {
     color: "red",
     backgroundColor: "green",
     textAlign: "center",
+    margin: "0"
   };
 
   return (
     <div>
       <h1 style={ElementOne}>Inline css</h1>
+      <p>This 2nd p</p>
+      <h2 className="title">Hello world</h2>
     </div>
   );
 }
