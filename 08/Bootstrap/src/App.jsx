1f844b77970bc570138becd81387d9d2fa217ddb
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-05-07 22:06:23 +0600

    update app jsx

--- 08/Bootstrap/src/App.jsx
+++ 08/Bootstrap/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Bootstrap from './Bootstrap'
+import Input from './Input'
 
 function App() {
   return (
     <>
     <Bootstrap/>
+    <hr />
+    <Input/>
     </>
   )
 }
