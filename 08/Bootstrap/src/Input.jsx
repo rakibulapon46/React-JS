0ddb15e74a6779d466688f4010d0c3375f25c57c
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-05-08 23:33:27 +0600

    onchange add input

--- 08/Bootstrap/src/Input.jsx
+++ 08/Bootstrap/src/Input.jsx
@@ -1,9 +1,12 @@
 
 function Input() {
+  const formChange = (event) => {
+    console.log(event.target.name)
+  }
   return (
-    <>
-    
-    </>
+    <div className="container">
+    <input type="text" name="firstName"placeholder="Enter your name here" className="form-control" onChange={formChange}/>
+    </div>
   )
 }
 
