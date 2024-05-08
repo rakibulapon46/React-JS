e542ede5f6102978134ae8a8b265eb16d2da9102
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-05-09 01:25:22 +0600

    select create

--- 08/Bootstrap/src/Input.jsx
+++ 08/Bootstrap/src/Input.jsx
@@ -6,6 +6,14 @@ function Input() {
   return (
     <div className="container">
     <input type="text" name="firstName"placeholder="Enter your name here" className="form-control" onChange={formChange}/>
+<br />
+    <select name="category" onChange={formChange} className="form-control">
+      <option value="Select">Select</option>
+      <option value="Programming">Programming</option>
+      <option value="Education">Education</option>
+      <option value="Entertainment">Entertainment</option>
+      <option value="Development">Development</option>
+    </select>
     </div>
   )
 }
