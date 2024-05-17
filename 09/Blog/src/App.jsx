59b149653370e6266aadd69096de54ae9d3d9da8
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-05-17 22:13:09 +0600

    add card

--- 09/Blog/src/App.jsx
+++ 09/Blog/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Card from './assets/BlogWebsite/Card'
 import Carousel from './assets/BlogWebsite/Carousel'
 import Navbar from './assets/BlogWebsite/Navbar'
 
@@ -9,6 +10,7 @@ function App() {
     <>
      <Navbar/>
      <Carousel/>
+     <Card/>
     </>
   )
 }
---
+++ 09/Blog/src/assets/BlogWebsite/Card.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+
+export class Card extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Card
