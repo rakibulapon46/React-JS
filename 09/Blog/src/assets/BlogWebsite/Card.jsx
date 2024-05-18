98b4d2ee4f9b30f6d6596e8d9398e66a956110df
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-05-18 21:20:01 +0600

    add card css

--- 09/Blog/src/assets/BlogWebsite/Card.jsx
+++ 09/Blog/src/assets/BlogWebsite/Card.jsx
@@ -1,20 +1,18 @@
-import { Component } from 'react'
+import "./BlogCSS/Card.css"
 
-export class Card extends Component {
-  render() {
-    return (
-      <div>
-        <div className="card" style="width: 18rem;">
-  <img src="..." className="card-img-top" alt="..."/>
+function Card(props) {
+  return (
+    <div className="container">
+      <div className="card">
+  <img src={props.img} className="card-img-top" alt="img"/>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">{props.title}</h5>
+    <p className="card-text">{props.text}</p>
+    <a href={props.href} className="btn btn-primary">{props.BtnText}</a>
   </div>
 </div>
-      </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Card
