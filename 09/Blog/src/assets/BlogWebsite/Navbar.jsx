6cdf53f4d2075faadbfff9b700a20a7ae70289db
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-05-20 05:09:01 +0600

    navlink add

--- 09/Blog/src/assets/BlogWebsite/Navbar.jsx
+++ 09/Blog/src/assets/BlogWebsite/Navbar.jsx
@@ -1,37 +1,31 @@
+import { NavLink, Link } from "react-router-dom";
+import "./BlogCSS/Blog.css";
 
 function Navbar() {
   return (
     <div>
         <nav className="navbar navbar-expand-md bg-body">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Blog Website</a>
+    <Link className="navbar-brand" to="/">Blog Website</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto mb-lg-0">
+      <ul className="navbar-nav ms-auto mb-lg-0 fs-5">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <NavLink className="nav-link" to="/">Home</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Blog
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+        <NavLink className="nav-link" to="/about">About</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Sponsor</a>
+        <NavLink className="nav-link" to="/blog">Blog</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
+        <NavLink className="nav-link" to="/sponsor">Sponsor</NavLink>
+        </li>
+        <li className="nav-item">
+        <NavLink className="nav-link" to="/contact">Contact</NavLink>
         </li>
       </ul>
       <form className="d-flex ms-5" role="search">
