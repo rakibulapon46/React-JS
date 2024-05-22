ad680b6f573cef6f94ad6b83b64f783c9956e2d5
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-05-23 03:19:51 +0600

    form add

--- 10/my-app/src/App.jsx
+++ 10/my-app/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react"
+// import { useState } from "react";
+
+import Form from "./Form";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  const [Name, setName] = useState("World")
-  const changeName = () =>{
-    setName("Apon")
-  }
+  // const [Name, setName] = useState("World");
+  // const changeName = () => {
+  //   setName("Apon");
+  // };
   return (
     <>
-    <h1>Hello {Name} </h1>
-    <button onClick={changeName}>Change name</button>
+      {/* <h1>Hello {Name} </h1>
+      <button onClick={changeName}>Change name</button> */}
+      <Form/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
