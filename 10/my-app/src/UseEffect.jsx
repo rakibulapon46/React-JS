907e2c0ab30061e66a70fae63291a434d9eee0c9
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-05-24 22:45:46 +0600

    useEffect part 2

--- 10/my-app/src/UseEffect.jsx
+++ 10/my-app/src/UseEffect.jsx
@@ -2,16 +2,20 @@ import {useEffect, useState} from 'react'
 
 function UseEffect() {
     const [count, setCount] = useState(0);
+    const [calculation, setCalculation] = useState(0)
     useEffect(() => {
         document.title = `Chat ${count}`;
-
-        setTimeout(() => {
-            setCount(count + 1)
-        }, 1000);
-    })
+        setCalculation(()=>{
+            setCalculation(count * 2)
+        })
+    }, [count])
   return (
     <div className='container'>
         <p>You have get {count} notification </p>
+        <p>Calculation: {calculation}</p>
+        <button className='btn btn-primary' onClick={()=>{
+            setCount(count + 1)
+        }}>Count me</button>
     </div>
   )
 }
