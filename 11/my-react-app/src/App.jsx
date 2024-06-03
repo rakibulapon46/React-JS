275dbc11df9c8fcbf5e5a44eadcd40a8376cecae
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-06-03 22:18:23 +0600

    add userGreetings

--- 11/my-react-app/src/App.jsx
+++ 11/my-react-app/src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
 import Student from './Student'
+import UserGreeting from './UserGreeting'
 
 function App() {
   return (
     <>
      <Student name="Apon" age={18} isStudent={true}/>
      <Student/>
+     <hr />
+     <UserGreeting isLoggedIn={true} username="BroCode" />
     </>
   )
 }
