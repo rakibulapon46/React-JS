b7e5502c5db6994a82f888aeb0db2f6e4728e6cc
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-06-07 21:39:31 +0600

    button with onclick handler

---
+++ 11/my-react-app/src/Button.jsx
@@ -0,0 +1,28 @@
+function Button() {
+
+//   const handleClick = () => console.log("Ouch!");
+//   const handleClick2 = (name) => console.log (`${name} stop click me`);
+
+    // let count = 0;
+
+    // const handleClick = (name) => {
+    //     if (count < 3) {
+    //         count++;
+    //         console.log(`${name} you clicked me ${count} time/s`);
+    //     } else {
+    //         console.log(`${name} stop clicked me`);
+    //     }
+    // }
+
+    const handleClick = (e) => e.target.textContent = "OUCH!"
+
+
+
+  return (
+    <>
+      <button onDoubleClick={(e) => handleClick(e)}>Click me</button>
+    </>
+  );
+}
+
+export default Button;
