c2ced4af7dceea822a478b7e0e42daf06484ed66
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-06-10 18:44:46 +0600

    update function add on counter

--- 11/my-react-app/src/Counter.jsx
+++ 11/my-react-app/src/Counter.jsx
@@ -4,11 +4,15 @@ function Counter() {
     const [count, setCount] = useState(0);
 
     const increment = () => {
-        setCount(count + 1);
+        setCount(count => count + 1);
+        setCount(count => count + 1);
+        setCount(count => count + 1);
     }
 
     const decrement = () => {
-        setCount(count - 1);
+        setCount(count => count - 1);
+        setCount(count => count - 1);
+        setCount(count => count - 1);
     }
 
     const reset = () => {
