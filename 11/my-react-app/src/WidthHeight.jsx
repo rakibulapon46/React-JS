cf16ee1969c24c50e88f7576af1db080e3b490df
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-06-16 00:47:22 +0600

    width height resizer

--- 11/my-react-app/src/WidthHeight.jsx
+++ 11/my-react-app/src/WidthHeight.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
 
 function WidthHeight() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    console.log("Event listener added");
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      console.log("Event listener remove");
+    }
+  }, []);
+
+  useEffect(()=> {
+    document.title = `Size: ${width} x ${height}` 
+  }, [width, height])
+
+  function handleResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+
   return (
-    <div>WidthHeight</div>
-  )
+    <div>
+      <p>Width: {width}px</p>
+      <p>Height: {height}px</p>
+    </div>
+  );
 }
 
-export default WidthHeight
+export default WidthHeight;
