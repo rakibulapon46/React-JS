1c24fee9d4d4a2eb8d3d8974b08e0537b153067a
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-06-19 23:45:05 +0600

    create useContext

--- 11/my-react-app/src/UseContext.jsx
+++ 11/my-react-app/src/UseContext.jsx
@@ -1,9 +1,19 @@
-import { useEffect, useState } from "react"
+import { useState, createContext } from "react"
+import UserContext2 from "./UserContext2"
+
+export const UserComponent = createContext();
 
 
 function UseContext() {
+  const [user,setUser] = useState("Apon")
   return (
-    <div>UseContext</div>
+    <div className="box">
+      <h1>UserComponent1</h1>
+      <h2>{`Hello ${user}`}</h2>
+      <UserComponent.Provider value={user}>
+      <UserContext2 user={user}/>
+      </UserComponent.Provider>
+    </div>
   )
 }
 
