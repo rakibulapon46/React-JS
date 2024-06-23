cf2dd9edbc32b306bcc08377fcac839c424356d0
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-06-23 23:59:22 +0600

    work on stopwatch

--- 11/my-react-app/src/StopWatch.jsx
+++ 11/my-react-app/src/StopWatch.jsx
@@ -1,7 +1,41 @@
+import { useState, useEffect, useRef } from "react"
 
 function StopWatch() {
+
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsdTime] = useState(0);
+  const intervalIdRef = useRef(null);
+  const startTimeRef = useRef(0);
+
+  useEffect(() => {
+
+  }, [isRunning]);
+
+  function start(){
+    
+  }
+
+  function stop(){
+
+  }
+
+  function reset(){
+
+  }
+
+  function formatTime(){
+    return `00:00:00`
+  }
+
   return (
-    <div>StopWatch</div>
+    <div className="stopwatch">
+      <div className="display">{formatTime()}</div>
+      <div className="controls">
+        <button onClick={start} className="start-button">Start</button>
+        <button onClick={stop} className="stop-button">Stop</button>
+        <button onClick={reset} className="reset-button">Reset</button>
+      </div>
+    </div>
   )
 }
 
