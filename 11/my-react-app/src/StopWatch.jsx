15ed00eacde239b89ce5e620d602d939492960db
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-06-24 21:50:08 +0600

    stopwatch done

--- 11/my-react-app/src/StopWatch.jsx
+++ 11/my-react-app/src/StopWatch.jsx
@@ -3,28 +3,50 @@ import { useState, useEffect, useRef } from "react"
 function StopWatch() {
 
   const [isRunning, setIsRunning] = useState(false);
-  const [elapsedTime, setElapsdTime] = useState(0);
+  const [elapsedTime, setElapsedTime] = useState(0);
   const intervalIdRef = useRef(null);
   const startTimeRef = useRef(0);
 
   useEffect(() => {
+    if (isRunning) {
+      intervalIdRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current)
+      }, 10);
+    }
 
+    return () => {
+      clearInterval(intervalIdRef.current);
+    }
   }, [isRunning]);
 
   function start(){
-    
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime;
+    console.log(startTimeRef.current)
   }
 
   function stop(){
-
+    setIsRunning(false)
   }
 
   function reset(){
-
+    setIsRunning(false)
+    setElapsedTime(0)
   }
 
   function formatTime(){
-    return `00:00:00`
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / (1000) % 60);
+    let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    return `${minutes}:${seconds}:${milliseconds}`
   }
 
   return (
